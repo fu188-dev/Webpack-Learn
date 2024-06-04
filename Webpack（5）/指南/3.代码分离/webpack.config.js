5794449859e234c399f6899b23429f45394a2767
fujing <156163947@qq.com>
2024-06-04 18:49:12 +0800

    feat 新增代码分离章节学习文件

---
+++ Webpack（5）/指南/3.代码分离/webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    // index: "./src/index.js",
+    // another: "./src/another-module.js"
+    index: {
+        import: "./src/index.js",
+        dependOn: "shared",
+    },
+    another: {
+        import: "./src/another-module.js",
+        dependOn: "shared",
+    },
+    shared: "lodash",
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: "Development",
+        filename: "index.html"
+    }),
+],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: { // 如果在一个 HTMl 页面上有多个入口起点，选哟设置 optimization.runtimeChunk: 'single'，否则会遇到 https://bundlers.tooling.report/code-splitting/multi-entry/ 这里描述的麻烦
+    runtimeChunk: "single",
+    splitChunks: {
+        chunks: "all",
+    },
+  },
+};
